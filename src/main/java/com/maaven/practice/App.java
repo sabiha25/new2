5b33bf50e7c81a8632f30e0fbedc391c5d9cee87
sabiha25 <sabihaks1025@gmail.com>
2019-05-15 11:56:16 +0530

    added pom.xml

---
+++ src/main/java/com/maaven/practice/App.java
@@ -0,0 +1,23 @@
+package com.maaven.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Hello World!" );
+
+
+        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World!" );
+
+    }
+}
